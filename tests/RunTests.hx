ea2a12b85f1ef1ff67e019d3b4daa8f52508a039
Juraj Kirchheim <back2dos@googlemail.com>
2016-05-12 12:55:40 +0200

    Fix the tests. Resolves #3.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -2,6 +2,22 @@ package;
 
 import buddy.*;
 
-class RunTests implements Buddy<[
-	TestIssue3,
-]> {}
+//class RunTests implements Buddy<[
+	//TestIssue3,
+//]> {}
+
+//TODO: figure out a way to make the above work
+
+import buddy.reporting.ConsoleReporter;
+
+class RunTests {
+    public static function main() {
+        var reporter = new ConsoleReporter();
+
+        var runner = new buddy.SuitesRunner([
+            new TestIssue3(),
+        ], reporter);
+
+        runner.run();
+    }
+}
