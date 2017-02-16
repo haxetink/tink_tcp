d155e16d789d774638d225cfbbdbe114daca89c5
back2dos <back2dos@gmail.com>
2017-02-16 15:02:58 +0100

    Use vscode + haxeshim.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -1,26 +1,7 @@
 package;
 
-import buddy.*;
-
-//class RunTests implements Buddy<[
-	//TestIssue3,
-//]> {}
-
-//TODO: figure out a way to make the above work
-
-import buddy.reporting.ConsoleReporter;
-
 class RunTests {
-    public static function main() {
-        var reporter = new ConsoleReporter();
-
-        var runner = new buddy.SuitesRunner([
-            new TestIssue3(),
-            #if (haxe_ver > 3.210) new TestSecureConnection(), #end
-        ], reporter);
-
-        runner.run().then(function (_) {
-          Sys.exit(if (runner.failed()) 500 else 0);
-        });
-    }
+  static function main() {
+    
+  }
 }
