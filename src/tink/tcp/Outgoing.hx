c9b6b528c04f0e896773d69c27d118065f540475
Kevin Leung <kevinresol@gmail.com>
2019-12-15 17:27:01 +0800

    A note about allowHalfOpen

--- src/tink/tcp/Outgoing.hx
+++ src/tink/tcp/Outgoing.hx
@@ -4,5 +4,8 @@ using tink.io.Source;
 
 typedef Outgoing = {
   var stream(default, never):IdealSource;
+  
+  // if true, handler promise resolves when outgoing stream is completely sent to remote
+  // otherwise it resolves when the incoming stream is also ended
   @:optional var allowHalfOpen(default, never):Bool;
 }
